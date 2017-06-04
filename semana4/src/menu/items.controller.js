6e892f413a4d444c493d5733b517b717804631dd
Carlos Viana <carlosjmviana@gmail.com>
2017-06-04 19:27:06 -0300

    cviana: fechando os items de verificação do exercício. Corrigindo nome do parâmetro que passa do controler para o template.

--- semana4/src/menu/items.controller.js
+++ semana4/src/menu/items.controller.js
@@ -9,7 +9,7 @@ ItemsController.$inject = ['$stateParams', 'MenuDataService', 'responseItems'];
 function ItemsController($stateParams, MenuDataService, responseItems) {
   var itemsOfCategory = this;
   itemsOfCategory.items = responseItems.data.menu_items;
-  itemsOfCategory.categoryShotName = $stateParams.shortName;
+  itemsOfCategory.categoryShortName = $stateParams.shortName;
 }
 
 })();
